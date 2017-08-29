2d139196cda94767f5d5bdb445b2fa9cee282821
Ubuntu <fayis@Ubuntu.zrjmsha2tvrelo2q352uml51se.jx.internal.cloudapp.net>
2017-08-29 18:52:18 +0000

    built added

--- server/app.js
+++ server/app.js
@@ -1,16 +1,42 @@
 var cmd = require('node-cmd');
 var express = require('express')
+var Promise =  require('bluebird');
+//import cmd from 'node-cmd'
+ 
+const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd })
+
 
 var app = express();
 app.use(express.static('public'));
 app.post('/gen', function(req, res){
 	console.log("okk");
-	cmd.run(`
-		cd ../
-		cd android
+/*	cmd.get(
+        `
+		cd ../android
 		./gradlew assembleRelease
-	`);
-	res.send("Done");
+        `,
+        function(err, data, stderr){
+            if (!err) {
+               console.log('the node-cmd cloned dir contains these files :\n\n',data)
+        	res.send("Done");    
+	} else {
+               console.log('error', err)
+            	res.send("err");
+		}
+        }
+);
+*/
+getAsync(`
+	cd ../android
+	./gradlew assembleRelease
+`).then(data => {
+  console.log('cmd data', data)
+}).catch(err => {
+  console.log('cmd err', err)
+});
+setTimeout(function(){
+	res.send("ok");
+},4000);
 })
 
 app.listen(3000, function(){
